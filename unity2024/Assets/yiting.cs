4f75ea6967fa211479e462770853ddc729efdb94
yitingdormouse <133759248+yitingdormouse@users.noreply.github.com>
2024-05-21 10:49:21 +0100

    unity workshop

---
+++ unity2024/Assets/yiting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yiting : MonoBehaviour
+{
+
+    public Rigidbody2D myRigidbody;
+    public float jump;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * jump;
+        }
+    }
+}
